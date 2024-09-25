6ef4d5019a791f7bb8818ba7bd37334a89a3bd2a
Bilment <tahabilmen@gmail.com>
2024-09-25 13:26:01 -0400

    initial commit

---
+++ workbook-1/first-maven-project/src/main/java/com/pluralsight/ShoppingList.java
@@ -0,0 +1,7 @@
+package com.pluralsight;
+
+public class ShoppingList {
+    public static void main(String[] args) {
+        System.out.println(" 1.Bread\n 2.Oil\n 3.Chicken\n 4.Meat\n 5.Fish\n 6.Salt\n 7.Avocado\n 8.Pasta\n 9.Rice\n 10.Cucumber");
+    }
+}
