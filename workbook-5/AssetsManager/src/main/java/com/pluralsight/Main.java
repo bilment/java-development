1cbec88bdc0a71d27269fbd857ae541c478c2f64
Bilment <tahabilmen@gmail.com>
2024-10-29 15:30:34 -0400

    Assets has been added

---
+++ workbook-5/AssetsManager/src/main/java/com/pluralsight/Main.java
@@ -0,0 +1,7 @@
+package com.pluralsight;
+
+public class Main {
+    public static void main(String[] args) {
+        House firstHouse = new House("Village", "Today", 300000.0, "500 Main St", 100, 1500, 50);
+    }
+}
