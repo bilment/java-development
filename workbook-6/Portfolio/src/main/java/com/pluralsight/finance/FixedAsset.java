db02ef6bc3c3118a5fd8029d0ae4e07785322d2c
Bilment <tahabilmen@gmail.com>
2024-10-31 13:47:14 -0400

    syntax has been modified

--- workbook-6/Portfolio/src/main/java/com/pluralsight/finance/FixedAsset.java
+++ workbook-6/Portfolio/src/main/java/com/pluralsight/finance/FixedAsset.java
@@ -1,8 +1,8 @@
 package com.pluralsight.finance;
 
 public abstract class FixedAsset implements Valuable {
-    protected String name;
-    protected double value;
+    public String name;
+    public double value;
 
     public FixedAsset(String name, double value) {
         this.name = name;
